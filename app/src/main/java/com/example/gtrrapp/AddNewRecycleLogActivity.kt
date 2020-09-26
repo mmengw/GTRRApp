dd4bd37baf959a472aeef15c3114a5e74dea690b
Rranddy <40783721+Rranddy@users.noreply.github.com>
2020-09-26 14:18:02 +0800

    working on the add news activity
    - this commit does not work

--- app/src/main/java/com/example/gtrrapp/AddNewRecycleLogActivity.kt
+++ app/src/main/java/com/example/gtrrapp/AddNewRecycleLogActivity.kt
@@ -2,15 +2,23 @@ package com.example.gtrrapp
 
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_add_recyclelog.*
+import kotlinx.android.synthetic.main.activity_register.*
+import org.w3c.dom.Text
 
 class AddNewRecycleLogActivity : AppCompatActivity(){
 
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mAuth: FirebaseAuth? = null
+
     lateinit var option: Spinner
+    lateinit var result: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,14 +33,32 @@ class AddNewRecycleLogActivity : AppCompatActivity(){
         option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long
             ) {
-
+                result.text = options.get(position)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
+        }
+    initialise()
+    }
+    private fun initialise(){
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.getReference("RecycleLog")
+        mAuth = FirebaseAuth.getInstance()
+    }
+    override fun onStart(){
+        super.onStart()
 
+        val mUser = mAuth!!.currentUser
+        val mUserReference = mDatabaseReference!!.child(mUser!!.uid)
+
+        updateBtn.setOnClickListener {
+            val recycleLog = RecycleLog("$mUserReference",recycleTitle.text.toString(),recycleDescription.text.toString(),"${result}")
+            mDatabaseReference!!.setValue(recycleLog)
         }
 
     }
 }
+
+class RecycleLog(val uid: String, val RTitle:String, val RDes:String, val RType:String)
--- app/src/main/java/com/example/gtrrapp/AdminAddNews.kt
+++ app/src/main/java/com/example/gtrrapp/AdminAddNews.kt
@@ -1,11 +1,90 @@
 package com.example.gtrrapp
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_recyclelog.*
+import kotlinx.android.synthetic.main.activity_add_recyclelog.updateBtn
+import kotlinx.android.synthetic.main.activity_admin_addnews.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.register_email
+import kotlinx.android.synthetic.main.activity_register.register_photobtn
+import kotlinx.android.synthetic.main.activity_register.register_userName
+import kotlinx.android.synthetic.main.activity_update_settings.*
+import java.util.*
 
-class AdminAddNews : AppCompatActivity(){
+class AdminAddNews : AppCompatActivity() {
+
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_addnews)
+
+        //ACTION FOR THE SELECT PHOTO BUTTON
+        cover_imagebtn.setOnClickListener {
+
+            //REDIRECT USER TO SELECT PICTURE FROM THE DOWNLOAD FOLDER
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+        //ACTION FOR THE UPDATE BUTTON
+        uploadBtn.setOnClickListener {
+                uploadImageToFirebaseStorage()
+        }
+    }
+
+    var selectedPhotoUri: Uri? = null
+
+    private fun uploadImageToFirebaseStorage(){
+        if (selectedPhotoUri == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref= FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener{
+                Log.d("AddNewsActivity", "Successfully uploaded image")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    it.toString()
+                    Log.d("AddNewsActivity", "File Location: $it")
+
+                    savNewsToFriebaseDatabase(it.toString())
+                }
+            }
+            .addOnFailureListener{
+                Log.d("AddNewsActivity","Fail to upload image")
+            }
+    }
+
+
+    private fun savNewsToFriebaseDatabase(NewsCoverImageUrl: String){
+
+        ref.setValue(news)
+            .addOnSuccessListener {
+                Log.d("AddNewsActivity", "Saved news Data to Firebase")
+
+            }
+            .addOnFailureListener{
+                Log.d("AddNewsActivity","Fail to upload news data")
+            }
     }
 }
+
+class News(val title:String, val link:String, val CoverImgUrl:String)
